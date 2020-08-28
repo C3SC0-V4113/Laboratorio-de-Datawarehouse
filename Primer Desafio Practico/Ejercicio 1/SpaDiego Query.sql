a5e08251d11e844f20a50b651d9d57d264fa6398
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-08-28 14:32:40 -0600

    Aver

--- Primer Desafio Practico/Ejercicio 1/SpaDiego Query.sql
+++ Primer Desafio Practico/Ejercicio 1/SpaDiego Query.sql
@@ -16,3 +16,8 @@ yoga bit
 )
 
 select*from Clientes
+SELECT TOP 100 id, (ingresos*promvisitas) As 'Ingresos por visitas', sauna, masaje, yoga, hidro FROM Clientes ORDER BY [Ingresos por visitas] DESC
+SELECT TOP 100 id, (ingresos*promvisitas) As 'Ingresos por visitas' FROM Clientes ORDER BY [Ingresos por visitas] DESC
+SELECT TOP 100 id,ingresos, promvisitas FROM Clientes where id='Belinda Parkin' Order by ingresos DESC
+select*from Clientes where id='Belinda Parkin'
+--DELETE FROM Clientes
