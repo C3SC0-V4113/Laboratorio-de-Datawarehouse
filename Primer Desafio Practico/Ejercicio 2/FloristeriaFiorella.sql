3671fdf13707d5c8f388e0f55cce20a8f8317b50
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-08-28 18:29:36 -0600

    Probando traslado a SQL
    Probando a demás funcion count en booleanos con un Aggregate, sino funciona pues la noche aun es joven

---
+++ Primer Desafio Practico/Ejercicio 2/FloristeriaFiorella.sql
@@ -0,0 +1,24 @@
+USE MASTER
+CREATE DATABASE Floristeria_Fiorella
+USE Floristeria_Fiorella
+
+CREATE TABLE Compras
+(
+id varchar(60),
+Rosas int,
+Claveles int,
+Macetas int,
+Tierra int,
+Girasoles int,
+Hortensia int,
+Globos int,
+Tarjetas int,
+Carmelias int,
+Orquidias int,
+Lirios int,
+Aurora int,
+Tulipanes int,
+Listón int
+)
+
+select*from Compras
